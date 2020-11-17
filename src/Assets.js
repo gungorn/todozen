3323d87e2995be860c4864ba15ac8aea853b7633
nurettin <gungorn@outlook.com>
2020-11-18 01:02:13 +0300

    assets

---
+++ src/Assets.js
@@ -0,0 +1,8 @@
+import logo_ from '../assets/images/logo/logo.png';
+export const logo = logo_;
+
+
+import splashback_ from '../assets/images/splashbackground.jpg';
+export const splashback = splashback_;
+
+
