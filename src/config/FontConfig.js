f9a3847ff81467ec3853693ab97539a680be4341
nurettin <gungorn@outlook.com>
2020-11-18 01:02:34 +0300

    FontConfig

---
+++ src/config/FontConfig.js
@@ -0,0 +1,12 @@
+export default {
+	XL: { fontSize: 24 },
+	L: { fontSize: 20 },
+	M: { fontSize: 16 },
+	S: { fontSize: 14 },
+	XS: { fontSize: 12 },
+	XL_BOLD: { fontSize: 24, fontWeight: 'bold' },
+	L_BOLD: { fontSize: 20, fontWeight: 'bold' },
+	M_BOLD: { fontSize: 16, fontWeight: 'bold' },
+	S_BOLD: { fontSize: 14, fontWeight: 'bold' },
+	XS_BOLD: { fontSize: 12, fontWeight: 'bold' },
+};
